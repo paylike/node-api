a536af248e345d7ab53d26bf03cc907467a0e876
Thomas Jensen <thomas@src.agency>
2016-01-19 20:53:21 +0100

    update agent header

--- src/index.js
+++ src/index.js
@@ -52,7 +52,7 @@ assign(Service.prototype, {
 	request: function( verb, path, query, cb ){
 		return fetch(verb, this.url+path, query, {
 			'Authorization': 'Basic ' + btoa(':'+this.key),
-			'X-Client': 'Node 0.0.8',
+			'X-Client': 'Node 1.0.0',
 		})
 			.catch(fetch.response.ClientError, function( e ){
 				if (e.code === 401)
